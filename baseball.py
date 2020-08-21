500daad2348594db4c3a1aeb52df5696a51c6691
Drew A. Dunn <dunnda1@gmail.com>
2020-08-21 13:58:18 -0400

    added League class, moving from original funtional approach to OO

--- baseball.py
+++ baseball.py
@@ -41,32 +41,7 @@ class Pitcher():
 		self.pso = ""
 		self.pbo = ""
 
-# reads csv files and store the data into the pandas dataframes
-# returns the 3 data frames
-def read_data():
-	batterDF = pd.read_csv("2017FanGraphsBatting.csv")
-	batterDF = batterDF.set_index("Name")
-	# TODO:
-	# There are duplicate Chris Young and Matt Duffy
-	# batterDF = batterDF.set_index("Name", verify_integrity=True)
-	batterCols = ["PA", "1B", "2B", "3B", "HR", "BB", "IBB", "HBP", "SO"]
-	batterDF[batterCols] = batterDF[batterCols].apply(pd.to_numeric,
-													  errors="ignore")
-	
-	pitcherDF = pd.read_csv("2017FanGraphsPitching.csv")
-	pitcherDF = pitcherDF.set_index("Name")
-	pitcherCols = ["TBF", "H", "2B", "3B", "HR", "BB", "IBB", "HBP", "SO"]
-	pitcherDF[pitcherCols] = pitcherDF[pitcherCols].apply(pd.to_numeric,
-													      errors="ignore")
-	singleCol = pitcherDF["H"] - (pitcherDF["2B"] + pitcherDF["3B"] 
-				+ pitcherDF["HR"])
-	pitcherDF["1B"] = singleCol
 
-	leagueDF = pd.read_csv("2017FanGraphsLeague.csv")
-	leagueDF = leagueDF.set_index("Season")
-	leagueDF[batterCols] = leagueDF[batterCols].apply(pd.to_numeric,
-													  errors="ignore")
-	return (batterDF, pitcherDF, leagueDF)
 
 # checks the input files containing team names and lineups and then creates and
 # returns lineups - a list of a string, 9 batter objects, and a pitcher object
@@ -103,6 +78,8 @@ def create_lineups(fileName1, fileName2, batterDF, pitcherDF):
 				lineup[i].name = tmplineup[i]
 		file.close()
 	return (lineup1, lineup2)
+
+
 
 # fills and returns attributes for the batter and pitcher objects in the lineup
 def fill_statline(batterDF, pitcherDF, leagueDF, lineup1, lineup2):
@@ -303,7 +280,11 @@ def playGame(lineup1, lineup2, leagueDF, brDF):
 			score = homeScore
 			battingOrder = homeBattingOrder
 		while out < 3:
-			playProbList = calcOddsRatio(batter, pitcher, leagueDF)
+			try:
+				playProbList = calcOddsRatio(batter, pitcher, leagueDF)
+			except:
+				err = 1
+
 			play = np.random.choice(playList, p=playProbList)
 			if play == "1b" or play == "2b" or play == "3b" or play == "bo":
 				brDict = brDF.loc[state, play]
@@ -380,22 +361,7 @@ def simulateHead2Head(lineupHome, lineupAway, leagueDF, brDF, ngames=10):
 
 	print("Total Wins -- {0}(Away): {1} | {2}(Home): {3}".format(awayTeam, awayWin, homeTeam, homeWin))
 
-def make_schedules(teams, length=24):
-	nominal_season_length = length
-	home_games = int(nominal_season_length / 2)
-	home_games_per_opp = int(home_games / (len(teams) - 1))
-	logger.info(f'Making schedule for teams:{teams}')
-	
-	logging.info(f'Home Games Per Opponent: {home_games_per_opp}')
 
-	schedule = dict()
-	for team in teams:
-		opp_teams = np.setdiff1d(teams, [team]).tolist()
-		games = opp_teams * home_games_per_opp
-		random.shuffle(games)
-		schedule[team] = games
-
-	return schedule
 
 def doHead2HeadSimulation(homeFile, awayFile):
 	
@@ -414,7 +380,7 @@ def getLineupFiles(teams):
 	
 	return lineupfiles
 
-def doSeasonSimulation(schedule):
+def doSeasonSimulation_old(schedule):
 
 	logger.info(f'Starting Season Simulation with teams {list(schedule.keys())}')
 	lineupfiles = getLineupFiles(list(schedule.keys()))
@@ -451,28 +417,308 @@ def doSeasonSimulation(schedule):
 				# homeWin += 1
 
 			results[winningTeam]['WINS'] += 1 
-			results[winningTeam]['LOSES'] += 1
+			results[losingTeam]['LOSES'] += 1
 
 			logger.info(f'{awayTeam} @ {homeTeam}: {winningTeam} wins, {awayScore} - {homeScore}')
 
 	return results
 
-def main(argv):
+
+class League():
+	def __init__(self,teams=None):
+
+		self.batterDF = None
+		self.pitcherDF = None
+		self.leagueDF = None
+		self.lineups = None
+		self.teams = teams	
+		self.standings = None
+
+	def get_standings(self):
+		return self.standings.sort_values(['W', 'GB'],ascending=False)
+
+
+	# reads csv files and store the data into the pandas dataframes
+	# returns the 3 data frames
+	def read_data(self):
+		self.batterDF = pd.read_csv("2017FanGraphsBatting.csv")
+		self.batterDF = self.batterDF.set_index("Name")
+		# TODO:
+		# There are duplicate Chris Young and Matt Duffy
+		# batterDF = batterDF.set_index("Name", verify_integrity=True)
+		batterCols = ["PA", "1B", "2B", "3B", "HR", "BB", "IBB", "HBP", "SO"]
+		self.batterDF[batterCols] = self.batterDF[batterCols].apply(pd.to_numeric,
+														errors="ignore")
+		
+		self.pitcherDF = pd.read_csv("2017FanGraphsPitching.csv")
+		self.pitcherDF = self.pitcherDF.set_index("Name")
+		pitcherCols = ["TBF", "H", "2B", "3B", "HR", "BB", "IBB", "HBP", "SO"]
+		self.pitcherDF[pitcherCols] = self.pitcherDF[pitcherCols].apply(pd.to_numeric,
+															errors="ignore")
+		singleCol = self.pitcherDF["H"] - (self.pitcherDF["2B"] + self.pitcherDF["3B"] 
+					+ self.pitcherDF["HR"])
+		self.pitcherDF["1B"] = singleCol
+
+		self.leagueDF = pd.read_csv("2017FanGraphsLeague.csv")
+		self.leagueDF = self.leagueDF.set_index("Season")
+		self.leagueDF[batterCols] = self.leagueDF[batterCols].apply(pd.to_numeric,
+														errors="ignore")
+	def load_lineup(self, team, filename):
+		
+		lineup = [None] * 11
+		with open(filename, 'r') as f:
+			
+			tmplineup = [s.strip() for s in f.readline().split(",")]
 	
-	mode = 'season_sim'
+			for i in range(11):
+				if i == 0:
+					lineup[i] = tmplineup[i]
+				elif i > 0 and i < 10:
+					if not tmplineup[i] in self.batterDF.index:
+						sys.exit("batter, " + tmplineup[i] + ", in " + fileName + 
+								(" does not exist in the FanGraphs database. "
+								"Please check the spelling of the player's "
+								"name."))
+					lineup[i] = Batter()
+					lineup[i].name = tmplineup[i]
+				else:
+					if not tmplineup[i] in self.pitcherDF.index:
+						sys.exit("pitcher, " + tmplineup[i] + ", in " + fileName + 
+								(" does not exist in the FanGraphs database. "
+								"Please check the spelling of the player's "
+								"name."))
+					lineup[i] = Pitcher()
+					lineup[i].name = tmplineup[i]
+		return lineup
+
+	# creates and returns a dataframe containing baserunning probabilities for each 
+	# state
+	def fill_baserunning(self):
+		logger.info('Populating base running')
+		playFile = open("2016plays.txt", "r")
+		playData = list(csv.reader(playFile))
+
+		# 24 possible states and 4 plays that we care about for baserunning stats
+		states = ["0,000","0,100","0,010","0,001","0,110","0,011","0,101","0,111",
+				"1,000","1,100","1,010","1,001","1,110","1,011","1,101","1,111",
+				"2,000","2,100","2,010","2,001","2,110","2,011","2,101","2,111"]
+		plays = ["1b", "2b", "3b", "bo"]
+
+		# initialize the dataframe with empty dictionaries
+		emptyDicts = [[{} for x in range(4)] for y in range(24)] 
+		self.brDF = pd.DataFrame(emptyDicts, index=states, columns=plays)
+
+		for index, row in enumerate(playData):
+			play = row[5]
+			if play == "2":
+				play = "bo"
+			elif play == "20":
+				play = "1b"
+			elif play == "21":
+				play = "2b"
+			elif play == "22":
+				play = "3b"
+
+			# only consider plays if 1B, 2B, 3B, or BO for the baserunning stats
+			if play == "bo" or play == "1b" or play == "2b" or play == "3b":
+				# figure out the current state
+				out = row[0]
+				if row[1] == "": 
+					base1 = "0"
+				else:
+					base1 = "1"
+				if row[2] == "":
+					base2 = "0"
+				else:
+					base2 = "1"
+				if row[3] == "":
+					base3 = "0"
+				else:
+					base3 = "1"
+				state = out + "," + base1 + base2 + base3
+
+				# update the out-count
+				newOut = int(out)
+				if row[6] == "0":
+					newOut += 1
+				if base1 == "1" and row[7] == "0":
+					newOut += 1
+				if base2 == "1" and row[8] == "0":
+					newOut += 1
+				if base3 == "1" and row[9] == "0":
+					newOut += 1
+
+				# update the bases situations and count any runs scored
+				newBase1 = "0"
+				newBase2 = "0"
+				newBase3 = "0"
+				newRuns = 0
+				for base in row[6:]:
+					if base == "1":
+						newBase1 = "1"
+					elif base == "2":
+						newBase2 = "1"
+					elif base == "3":
+						newBase3 = "1"
+					elif base == "4" or base == "5" or base == "6":
+						newRuns += 1
+				newState = (str(newOut) + "," + newBase1 + newBase2 + newBase3
+							+ "," + str(newRuns))
+
+				# update the number of occurrences of new state in the dictionaries
+				brDict = self.brDF.loc[state, play]
+				if "total" in brDict:
+					brDict["total"] += 1
+				else:
+					brDict["total"] = 1
+				if newState in brDict:
+					brDict[newState] += 1
+				else:
+					brDict[newState] = 1
+
+
+	def init_season(self, year=2017):
+		if self.teams is None:
+			raise ValueError('No Teams Defined')
+
+		self.set_schedules()
+		self.read_data()
+		self.fill_baserunning()
+
+		self.standings = pd.DataFrame(index=list(self.schedule.keys()), columns = ['W', 'L', 'GB'])
+		self.standings.fillna(0, inplace=True)
+		self.standings = self.standings.assign(GB=np.nan)
+
+		self.lineups = dict()		
+		for team in self.schedule:
+			# self.standings[team] = {'WINS': 0, 'LOSES': 0}
+			self.lineups[team] = self.load_lineup(team, f'sample_lineup_{team.lower()}_2017.txt')
+
+
+	def set_schedules(self, length=24, method="random"):
+		
+		if method == "random":
+			nominal_season_length = length
+			home_games = int(nominal_season_length / 2)
+			home_games_per_opp = int(home_games / (len(self.teams) - 1))
+			logger.info(f'Making schedule for teams:{self.teams}')
+			
+			logging.info(f'Home Games Per Opponent: {home_games_per_opp}')
+
+			self.schedule = dict()
+			for team in self.teams:
+				opp_teams = np.setdiff1d(self.teams, [team]).tolist()
+				games = opp_teams * home_games_per_opp
+				random.shuffle(games)
+				self.schedule[team] = games
+		else:
+			raise ValueError(f"Unknown method for setting schedules, '{method}'")
+
+# returns lineup based on team specified - a list of a string, 9 batter objects, and a pitcher object
+	def get_lineups(self, team):
+		# lineup1, lineup2 = [None] * 11, [None] * 11
+		# lineupList = [lineup1, lineup2]
+		# fileList = [fileName1, fileName2]
+		lineup = [None]*11
+		# for lineup, fileName in zip(lineupList, fileList):
+		# 	if not fileName.lower().endswith(".txt"):
+		# 		sys.exit("Sorry, lineup file, " + fileName + (" needs to be in a "
+		# 				".txt file"))
+		# 	file = open(fileName, "r")
+		# 	tmplineup = [s.strip() for s in file.readline().split(",")]
+
+		for i in range(11):
+			if i == 0:
+				lineup[i] = tmplineup[i]
+			elif i > 0 and i < 10:
+				if not tmplineup[i] in self.batterDF.index:
+					sys.exit("batter, " + tmplineup[i] + ", in " + fileName + 
+							(" does not exist in the FanGraphs database. "
+							"Please check the spelling of the player's "
+							"name."))
+				lineup[i] = Batter()
+				lineup[i].name = tmplineup[i]
+			else:
+				if not tmplineup[i] in pitcherDF.index:
+					sys.exit("pitcher, " + tmplineup[i] + ", in " + fileName + 
+							(" does not exist in the FanGraphs database. "
+							"Please check the spelling of the player's "
+							"name."))
+				lineup[i] = Pitcher()
+				lineup[i].name = tmplineup[i]
+			# file.close()
+		# return (lineup1, lineup2)
+		return lineup
+		
+
+	def simulate_season(self):
+
+		logger.info(f'Starting Season Simulation with teams {list(self.schedule.keys())}')
+		# lineupfiles = getLineupFiles(list(schedule.keys()))
+
+		# batterDF, pitcherDF, leagueDF = read_data()
+
+		# results = dict()
+		# for team in schedule:
+		# 	results[team] = {'WINS': 0, 'LOSES': 0}
+
+
+		for homeTeam in self.schedule:
+			# if not homeTeam in results:
+			# 	results[homeTeam] = {'WINS': 0, 'LOSES': 0}
+			homeLineup = self.lineups[homeTeam]
+			opponents = self.schedule[homeTeam]
+			# homeFile = lineupfiles[homeTeam]
+			for awayTeam in opponents:
+				# awayFile = lineupfiles[awayTeam]
+				
+				awayLineup = self.lineups[awayTeam]
+				homeLineup, awayLineup, self.leagueDF = fill_statline(self.batterDF, self.pitcherDF, self.leagueDF, homeLineup, awayLineup)
+				self.brDF = fill_baserunning()
+
+
+				result, awayScore, homeScore = playGame(homeLineup, awayLineup, self.leagueDF, self.brDF)
+				if result == 0:
+					winningTeam = awayTeam
+					losingTeam = homeTeam
+					# awayWin += 1
+				else:
+					winningTeam = homeTeam
+					losingTeam = awayTeam
+					# homeWin += 1
+
+				# self.standings[winningTeam]['W'] += 1 
+				# self.standings[losingTeam]['L'] += 1
+				self.standings.loc[winningTeam, 'W'] += 1
+				self.standings.loc[losingTeam, 'L'] += 1
+
+				logger.info(f'{awayTeam} @ {homeTeam}: {winningTeam} wins, {awayScore} - {homeScore}')
+
+		
+
+
+def main(mode='season_sim', season_length=4):
+	
 
 	if mode == 'head_to_head_sim':
-		homeLineupFile = argv[1]
-		awayLineupFile = argv[2]
-		doHead2HeadSimulation(homeLineupFile, awayLineupFile)
+		logger.error('Head-to-Head Simulation not working')
+		# homeLineupFile = argv[1]
+		# awayLineupFile = argv[2]
+		# doHead2HeadSimulation(homeLineupFile, awayLineupFile)
 
 	elif mode == 'season_sim':
 		teams = ['Cardinals', 'Nationals', 'Dodgers', 'Astros']
-		schedule = make_schedules(teams, length=162)
-		results = doSeasonSimulation(schedule)
-		pp.pprint(results)
+		# schedule = make_schedules(teams, length=162)
+		
+		league = League(teams=teams)
+		league.init_season()
+		league.simulate_season()
+		# doSeasonSimulation(schedule)
+		standings = league.get_standings()
+
+		print('Final Season Standings:')		
+		pp.pprint(standings)
 
 	
-if __name__ == "__main__": main(sys.argv)
-
-
+if __name__ == "__main__": 
+		main()
